2d121910bf71c40f4eade94d49cd827e4e1d71cf
David <paperdavid@gmail.com>
2016-06-02 14:51:04 +0300

    finish

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,16 +6,14 @@ $(document).ready(function() {
 
 		/* Custom */
 
-		//= ./common/likely.js
 		//= ./common/owl.carousel.min.js
-		//= ./common/jquery.chocolate.js
+		//= ./common/blueimp-gallery.js
+		//= ./common/blueimp-gallery-fullscreen.js
 
 
-  $('.chocolat-parent').Chocolat();
 
- $('.carousel').carousel();
 
-	$('.loop').owlCarousel({
+	$('.main-page').owlCarousel({
 			center: true,
 			items:1,
 			loop:true,
@@ -24,24 +22,55 @@ $(document).ready(function() {
 			autoplaySpeed:1000,
 			navSpeed: 1000,
 			dotsSpeed: 1000,
+			animateOut: 'fadeOut',
+			mouseDrag: false
 	});
 
-	$('.about').owlCarousel({
+	$('.artist').owlCarousel({
 			center: true,
-			items:3,
-			autoplay:2000,
+			items:1,
 			loop:true,
-			margin:10,
-			responsive:{
-						0:{
-								items:1
-						},
-						600:{
-								items:2
-						},
-						1000:{
-								items:3
-						}
-			}
+			autoplay:3000,
+			margin:100,
+			autoplaySpeed:1000,
+			navSpeed: 1000,
+			dotsSpeed: 1000,
+			animateOut: 'fadeOut',
+			mouseDrag: false
+})
+
+	$('.exhibition').owlCarousel({
+			center: true,
+			items:1,
+			loop:true,
+			autoplay:3000,
+			margin:100,
+			autoplaySpeed:1000,
+			navSpeed: 1000,
+			dotsSpeed: 1000,
+			animateOut: 'fadeOut',
+			mouseDrag: false
 	});
+
+	$('.team-group').owlCarousel({
+			center: true,
+			items:1,
+			loop:true,
+			autoplay:3000,
+			margin:100,
+			autoplaySpeed:1000,
+			navSpeed: 1000,
+			dotsSpeed: 1000,
+			animateOut: 'fadeOut',
+			mouseDrag: false
+	});
+
+	document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
 });
